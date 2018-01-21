2f240832ea50a0a068fba6e497e4fb08a951db14
torreswoo <torreswoo@gmail.com>
2018-01-22 01:05:41 +0900

    logic | add 1~4 methods

--- src/main/java/com/torres/service/RedisService.java
+++ src/main/java/com/torres/service/RedisService.java
@@ -1,16 +1,27 @@
 package com.torres.service;
 
 
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class RedisService {
 
     @Resource(name = "redisTemplate")
+    private RedisOperations redisOperations;
+
+    @Resource(name = "redisTemplate")
     private ValueOperations<String, String> valusOps;
+
+    @Resource(name = "redisTemplate")
+    private SetOperations<String, String> setOperations;
 
     public Long getVisitCount() {
         Long count = 0L;
@@ -22,4 +33,65 @@ public class RedisService {
         }
         return count;
     }
+
+    // 1.
+    public Set<String> getFriends(String user){
+
+        Set<String> friends = setOperations.members("user:"+user+":friends");
+        return friends;
+    }
+
+    // 2.
+    public boolean areFirstDegree(String userA, String userB){
+
+        Set<String> friends = setOperations.members("user:"+userA+":friends");
+
+        return checkUserInFriendlist(userB, friends);
+    }
+
+    // 3.
+    public boolean areSecondDegree(String userA, String userB){
+        if (areFirstDegree(userA, userB) == true)
+            return false;
+
+        Set<String> firstDegreeFriends = setOperations.members("user:"+userA+":friends");
+        Set<String> secondDegreeFriends = findNextDegreeFriends(firstDegreeFriends);
+
+        return checkUserInFriendlist(userB, secondDegreeFriends);
+    }
+
+
+    // 4.
+    public boolean areThirdDegree(String userA, String userB){
+        if (areSecondDegree(userA, userB) == true)
+            return false;
+
+        Set<String> firstDegreeFriends = setOperations.members("user:"+userA+":friends");
+        Set<String> secondDegreeFriends = findNextDegreeFriends(firstDegreeFriends);
+        Set<String> thirdDegreeFriends = findNextDegreeFriends(secondDegreeFriends);
+
+        return checkUserInFriendlist(userB, thirdDegreeFriends);
+    }
+
+
+    //
+    private boolean checkUserInFriendlist(String userB, Set<String> friends) {
+        Set<String> filter = friends
+            .stream()
+            .filter(friend -> friend.equals(userB))
+            .collect(Collectors.toSet());
+        return filter.size() != 0 ? true : false;
+    }
+
+    private Set<String> findNextDegreeFriends(Set<String> firstDegreeFriends) {
+        Set<String> secondDegreeFriends = new HashSet<String>();
+        firstDegreeFriends.forEach(friend -> {
+            secondDegreeFriends.addAll(setOperations.members("user:"+friend+":friends"));
+        });
+        return secondDegreeFriends;
+    }
+
+
+
+
 }
---
+++ src/test/java/com/torres/service/RedisServiceTest.java
@@ -0,0 +1,109 @@
+package com.torres.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class RedisServiceTest {
+
+
+    @InjectMocks
+    private RedisService redisService;
+
+    @Resource(name = "redisTemplate")
+//    @Mock
+    private SetOperations<String, String> setOperations;
+
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        assertThat(redisService).isNotNull();
+
+        List<List<String>> friends = new ArrayList<List<String>>();
+        List<String> friend01 = new ArrayList<String>();friend01.add("Richard");friend01.add("Anthony");
+        List<String> friend02 = new ArrayList<String>();friend02.add("Mina");   friend02.add("Marc");
+        List<String> friend03 = new ArrayList<String>();friend03.add("Mina");   friend03.add("Zuny");
+        friends.add(friend01);
+        friends.add(friend02);
+        friends.add(friend03);
+
+        friends.stream().forEach(friend -> {
+
+            String userA = friend.get(0);
+            String userB = friend.get(1);
+
+            setOperations.add("user:"+userA+":friends", userB);
+            setOperations.add("user:"+userB+":friends", userA);
+        });
+    }
+
+
+    @Test
+    public void test(){
+
+        Set<String> str = setOperations.members("user:Mina:friends");
+
+        str.forEach(s -> System.out.println(s));
+
+
+        // 02.
+        String userA = "Mina";
+        String userB = "Torres";
+        Set<String> friends = setOperations.members("user:"+userA+":friends");
+        Set<String> filter = friends.stream().filter(friend -> friend.equals(userB)).collect(Collectors.toSet());
+        System.out.println("-----------"+filter.size());
+        boolean result = filter.size() != 0 ? true : false;
+        System.out.println("-----------"+result);
+
+        // 03
+        Set<String> firstDegreeFriends = setOperations.members("user:"+userA+":friends");
+        Set<String> secondDegreeFriends = new HashSet<String>();
+        firstDegreeFriends.forEach(friend -> {
+            secondDegreeFriends.addAll(setOperations.members("user:"+friend+":friends"));
+        });
+
+        boolean resu = secondDegreeFriends.size() != 0 ? true : false;
+
+
+    }
+
+//    @Test
+//    public void test_getFriends(){
+//
+//        // getFriends
+//        String userA = "Mina";
+//        Set<String> friends = new HashSet<String>();
+//        friends.add("Marc");
+//        friends.add("Zuny");
+//        when(setOperations.members("user:"+userA+":friends")).thenReturn(friends);
+//
+//        Set<String> friendsOfUserA = redisService.getFriends(userA);
+//        assertThat(friendsOfUserA.size()).isEqualTo(2);
+//
+//    }
+
+
+}
